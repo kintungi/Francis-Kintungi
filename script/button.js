fb591a8fbc30cc9a5b01096148c173749b23cf62
Francis Kintungi <44190158+kintungi@users.noreply.github.com>
2020-08-01 08:57:20 +0300

    Delete button.js

--- script/button.js
+++
@@ -1,6 +0,0 @@
-var menuBtn = document.getElementById("menu-button");
-function toggleMenu() {
-    menuBtn.classList.toggle("tapped");
-};
-menuBtn.addEventListener("click", toggleMenu, false);
-console.log(menuBtn)
